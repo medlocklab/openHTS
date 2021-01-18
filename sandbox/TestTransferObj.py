5a9f4977ede390ea925ef37d9eed61f95ec43b24
gregmedlock <gmedlo@gmail.com>
2021-01-18 12:21:17 -0500

    updated environment.yml with pyserial==3.5 which is required for opentrons compatibility with big sur.

--- sandbox/TestTransferObj.py
+++ sandbox/TestTransferObj.py
@@ -1,11 +1,12 @@
 import pipette
 
-#mypipette = pipette.Transfer()
+mypipette = pipette.Transfer()
+from opentrons import protocol_api
 
+metadata = {'apiLevel': '2.8'}
+mypipette.tiprack_name = 'opentrons_96_tiprack_300ul'
 
-#mypipette.tiprack_name = 'geb_96_tiprack_1000ul'
-
-pipette.tuberack1_pos = 1
-#mypipette.tuberack2_pos = 1
-pipette.to96(maxVol=200, filepath='/home/andee/uva_Opentrons/random_plate.tsv')
+mypipette.tuberack1_pos = 1
+mypipette.tuberack2_pos = 2
+mypipette.to96(maxVol=200, filepath='../test/generated_test_files/random_plate.tsv')
 
